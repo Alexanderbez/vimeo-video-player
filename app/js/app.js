b0e62667dd21451bcb24fe7dc7ed648b1f6e29f7
Alex Bezobchuk <abezobchuk@jibe.com>
2016-04-05 17:59:41 -0400

    Incremental changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,19 @@
 'use strict';
 
 (function(window, document) {
-  let videoEl = document.getElementById('video-player');
-  let videoPlayer = new VideoPlayer(videoEl);
+    let videoEl = document.getElementById('video-player');
+    let videoPlayer = new VideoPlayer(videoEl);
 
-  videoPlayer.init();
+    /*
+      Set video player attributes such as the controls and various sections.
+      These attributes should not change in structure, so we can store them
+      without worry.
+    */
+    videoPlayer.setAttr('videoControls', document.getElementById('video-controls'));
+    videoPlayer.setAttr('playButton', document.getElementById('play'));
+    videoPlayer.setAttr('videoProgress', document.getElementById('video-progress'));
+    videoPlayer.setAttr('videoProgressBox', document.getElementById('video-box'));
+    videoPlayer.setAttr('fsButton', document.getElementById('full-screen'));
+
+    videoPlayer.init();
 }(this, document));
--- app/js/videoPlayer.js
+++ app/js/videoPlayer.js
@@ -5,24 +5,47 @@
  * features can be controlled.
  */
 class VideoPlayer {
-  /**
-   * VideoPlayer class constructor.
-   * 
-   * @player {Object} video player DOM object.
-   * @return {VideoPlayer} returns an instance of a VideoPlayer.
-   */
-  constructor(player) {
-    this.player = player;
-  }
+    /**
+     * VideoPlayer class constructor.
+     *
+     * @player {Object} video player DOM object.
+     * @return {VideoPlayer} returns an instance of a VideoPlayer.
+     */
+    constructor(player) {
+        this.player = player;
+        this.attributes = new Map();
+    }
 
-  /**
-   * TODO.
-   * 
-   * @return {TODO} TODO.
-   */
-  init() {
-    // 
-    this.player.removeAttribute('controls');
-    // video.addEventListener('loadeddata', this.initializeControls, false);
-  }
+    /**
+     * Sets an attribute of a VideoPlayer by a given name with a specific
+     * value.
+     *
+     * @name {String} The name to which associate the value with.
+     * @value {Object} The desired value to store.
+     * @return {Object} The value that was set.
+     */
+    setAttr(name, value) {
+        this.attributes.set(name, value);
+        return value;
+    }
+
+    /**
+     * Gets an attribute of a VideoPlayer by a given name.
+     *
+     * @name {String} The name to which to query for.
+     * @return {Object} The value that was retrieved.
+     */
+    getAttr(name) {
+        return this.attributes.get(name);
+    }
+
+    /**
+     * Performs various initialization operations on the video player itself
+     * upon being created.
+     *
+     * @return {Object} .
+     */
+    init() {
+        video.addEventListener('loadeddata', this.initializeControls, false);
+    }
 }
